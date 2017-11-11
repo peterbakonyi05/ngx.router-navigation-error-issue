620e407ddd46b7c4972f020949117e8cffaaed4c
BETSSON\peba02 <peter.bakonyi@betssongroup.com>
2017-11-11 16:01:55 +0100

    add home and not found pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
