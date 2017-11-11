ee2c94ede530b40a8d240934a74efed5baf89a4c
BETSSON\peba02 <peter.bakonyi@betssongroup.com>
2017-11-11 16:07:15 +0100

    throw error in `ngOnDestroy`

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   encapsulation: ViewEncapsulation.None
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    throw new Error('Kill the app');
+  }
 }
